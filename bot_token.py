ebc1aa41cb360998a6d69224986e04fa87ab8b3e
HAIRTIME <74730764+HAIRTIME@users.noreply.github.com>
2020-12-07 03:30:08 -0600

    Create bot_token.py

---
+++ bot_token.py
@@ -0,0 +1,8 @@
+#add your bot's authentication token in quotes like this => "ID goes here" for the token variable
+#add your channel ID without quotes for the my_channel variable
+#put this file in the same folder as the Discord_bot.py file
+#you may manually add this information in place of the variables to the Discord.py file, but I advise against it
+#IT'S HAIRTIME!!
+
+token = ""
+my_channel = 
